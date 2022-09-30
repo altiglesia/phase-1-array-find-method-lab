88884755858e45612b0b2ee2746352943e133fb9
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-09-30 14:28:26 -0400

    commit tree 85e8134e1c5fc030a8071a006a41e4f3a477df3c

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code your solution here
+function superbowlWin(recordArray) {
+    const result = recordArray.find(recordArray => recordArray.result === 'W');
+    return !!result ? result.year: undefined;
+}
